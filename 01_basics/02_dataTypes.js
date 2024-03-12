cad4e77b4862b9e2f51630b6aeb46d573723e11d
Vivek Dhawale <132386169+vickss07@users.noreply.github.com>
2024-03-12 11:21:53 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(3+3) //we are using node.js not broweser 
+
+console.log(3+3)
+
+let name = "vicky" //string
+let age = 3 //number 
+let isLogged = false //boolean
+let state;  // undefined
+
+//number => 2 to power 53
+//bigint
+//string = ""
+//boolean => true/false
+//null => standalone value
+// undefined 
+// symbol => unique
+
+
+//object
+
+console.log(typeof age) // number 
+console.log(typeof null) // object 
+console.log(typeof undefined) // undefined
